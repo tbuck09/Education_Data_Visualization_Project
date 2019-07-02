85268178b73fb883db5e4a71d80c6faff48b614d
tbuck09 <tylerbuck09@gmail.com>
2019-07-02 15:57:01 -0600

    Changes made to Flask and JS to reflect 2013

--- app.py
+++ app.py
@@ -44,17 +44,23 @@ def serve_demo_math(db_request):
 
     ela_demog= mongo.db.ela_demog
     math_demog= mongo.db.math_demog
-    ela_2015= mongo.db.ela_2015
-    math_2015= mongo.db.math_2015
+    ela_2013= mongo.db.ela_2013
+    math_2013= mongo.db.math_2013
+    ela_2013_agg= mongo.db.ela_2013_agg
+    math_2013_agg= mongo.db.math_2013_agg
     
     if db_request == "ela_demog":
         selected_db= ela_demog
     elif db_request == "math_demog":
         selected_db= math_demog
-    elif db_request == "ela_2015":
-        selected_db= ela_2015
-    elif db_request == "math_2015":
-        selected_db= math_2015
+    elif db_request == "ela_2013":
+        selected_db= ela_2013
+    elif db_request == "math_2013":
+        selected_db= math_2013
+    elif db_request == "ela_2013_agg":
+        selected_db= ela_2013_agg
+    elif db_request == "math_2013_agg":
+        selected_db= math_2013_agg
     else:
         return """
         <h1>Error:</h1>
@@ -69,11 +75,8 @@ def serve_demo_math(db_request):
 
     for result in results:
         output.append({
-            "name": result["leaname"],
             "state": result["stateabb"],
             "grade": result["grade"],
-            "year": result["year"],
-            "subject": result["subject"],
             "mean_score": result["mn_all"]
         })
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 url= "127.0.0.1:5000"
-requested_db= "ela_2015"
+requested_db= "ela_2013_agg"
 
 console.log(`${url}/${requested_db}`)
 
